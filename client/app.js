315dde8ae8f2da8beee902a38573e2bb82acbbc5
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-05-03 12:01:57 -0500

    login

--- client/app.js
+++ client/app.js
@@ -22,13 +22,14 @@ angular.module("Stubtrax", ['ngRoute']).config($routeProvider => {
 });
 
 // On a page refresh, the currentUser we set in the auth factory is lost, since it's just a variable. We need to be able to ask the backend for the user it has stored in session so we can reestablish who our current user is. Below, we listen for a route change and call a method that will ping the backend for the logged-in user, then broadcast that information via a custom event to the listening controllers ( see the test controller and the movie controller)
+// function(event, next)
 angular
   .module("Stubtrax")
   .run(($rootScope, $location, $route, $window, AuthFactory) => {
-    $rootScope.$on("$routeChangeStart", function(event, next) {
+    $rootScope.$on("$routeChangeStart", function() {
       AuthFactory.setUserStatus().then(() => {
         console.log("user", AuthFactory.getCurrentUser());
-        console.log("next", next);
+        // console.log("next", next);
         AuthFactory.broadcastUserLogin(AuthFactory.getCurrentUser());
       });
     });
